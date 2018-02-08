dae69015b26164620c63527b01603e45e97684e5
Anna Rúbies Caba <annarc3@tr12.cat>
2018-02-08 11:47:24 +0100

    class modification

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+
+function init(callback){
+    consoloe.log("entra init");
+    callback();
+}
+
+function sayHello(){
+    console.log("Hello!");
+    alert("Hello!");
+}
+
+function sayHi(){
+    console.log()
+}
+
+init(sayHello);
